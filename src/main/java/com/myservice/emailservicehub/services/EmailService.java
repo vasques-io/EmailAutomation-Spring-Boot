4094fdb56c79bebf7208d40e9d633a65a7fd8242
Felipe Vasques <felipeovgh@gmail.com>
2023-09-20 22:17:33 -0400

    Aplicação base feita com sucesso!

--- src/main/java/com/myservice/emailservicehub/services/EmailService.java
+++ src/main/java/com/myservice/emailservicehub/services/EmailService.java
@@ -4,9 +4,14 @@
  */
 package com.myservice.emailservicehub.services;
 
+import com.myservice.emailservicehub.enums.StatusEmail;
 import com.myservice.emailservicehub.models.EmailModel;
 import com.myservice.emailservicehub.repositories.EmailRepository;
+import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,8 +24,25 @@ public class EmailService {
     @Autowired
     EmailRepository emailRepository;
 
-    public void sendEmail(EmailModel emailModel) {
-        
+    @Autowired
+    private JavaMailSender emailSender;
+    
+    public EmailModel sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+            
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e) {
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(emailModel);
+        }
     }
     
 }
